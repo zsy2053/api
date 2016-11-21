0d0589936c124362a7ce9326ff5a29efe15daed0
Greg Markou <gregorymarkou@gmail.com>
2016-11-21 01:21:15 -0500

    Added cars

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517171442) do
+ActiveRecord::Schema.define(version: 20161121015718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.integer  "year"
+    t.string   "make"
+    t.string   "model"
+    t.integer  "km"
+    t.string   "tran"
+    t.string   "fuel_type"
+    t.integer  "price"
+    t.string   "plate_num"
+    t.string   "color"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
+    t.string   "address"
+    t.string   "age"
+    t.string   "gender"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
