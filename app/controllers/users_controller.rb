665da5717c7f91fd8ddd716450a36fa3ce00a9e2
Greg Markou <gregorymarkou@gmail.com>
2016-12-09 15:38:41 -0500

    Fixed user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: {}, status: 200
+      render json: {user: user}, status: 200
     else
       render json: ErrorSerializer.serialize(user.errors), status: 422
     end
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
 
 
