bee6069772c5575e72e4610f3f66e03c2795cf89
zsy2053 <zsy2053@gmail.com>
2017-02-11 14:34:43 -0500

    Fix the profile image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205185121) do
+ActiveRecord::Schema.define(version: 20170211191146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,10 +71,12 @@ ActiveRecord::Schema.define(version: 20170205185121) do
     t.string   "age"
     t.string   "gender"
     t.string   "password"
+    t.string   "image"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.boolean  "admin"
     t.string   "password_confirmation"
+    t.string   "avatar"
   end
 
 end
