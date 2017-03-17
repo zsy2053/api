58d981873648dbd1f0f8044c1c511ddfcdc114f0
Greg Markou <gregorymarkou@gmail.com>
2017-03-16 20:39:44 -0400

    added register_complete to the accepted params for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:id, :email, :password, :password_confirmation, :gender, :age, :image, :first_name, :last_name)
+      params.require(:user).permit(:id, :email, :password, :password_confirmation, :gender, :age, :image, :first_name, :last_name, :register_complete)
     end
 
 
